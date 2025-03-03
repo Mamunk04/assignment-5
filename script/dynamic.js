0d40c06bef8daa2c530d6ca3c736241c51f006df
Mūhàmmad Màmûñ Khàñ <khan110167@gmail.com>
2025-03-03 22:38:22 +0600

    complete

--- script/dynamic.js
+++ script/dynamic.js
@@ -1,6 +1,5 @@
 const themeBtn = document.getElementById("theme-btn");
 
-
 themeBtn.addEventListener("click" , function(){
     let mathColor = Math.random()*360
     let colorCode = Math.floor(mathColor);
@@ -10,3 +9,25 @@ themeBtn.addEventListener("click" , function(){
     document.body.style.backgroundColor = color;
     
 })
+
+
+const discoverCard = document.getElementById("discover-card");
+discoverCard.addEventListener("click",  function(){
+    window.location.href ='./Blog.html'
+})
+
+
+let newDate = new Date();
+let newYear = newDate.getFullYear();
+let month = newDate.getMonth();
+let date = newDate.getDate();
+let day = newDate.getDay();
+console.log(day)
+let months = [
+    "Jan", "Feb", "Mar", "Apr", "May", "Jun", 
+    "Jul", "Aug", "Sep", "Oct", "Novem", "Dec"
+];
+let daysOfWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+ let thisDate = daysOfWeek[day] + " " + months[month] + " " + date +" "+newYear;
+ document.getElementById("date").innerText = thisDate
